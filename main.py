b1a1bb2e807a47c87b59ba32142edd70c488ae4c
Nipun Mendes <nipunmendes@gmail.com>
2024-06-27 20:48:47 +0530

    app routes

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from flask import Flask, request,render_template
-
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-#python main
+#create routes
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-if __name__=="__main__":
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+#python main
+if __name__ == "__main__":
     app.run(debug=True)
